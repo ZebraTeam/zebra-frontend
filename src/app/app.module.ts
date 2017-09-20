a70390603d6b03932b2f3a0a573812e72b772548
Paweł Jan Czochański <czochanski@gmail.com>
2017-09-20 21:46:38 +0200

    Add router capability
    The router will be used to create multiple different views under
    different addresses in Angular.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './not-found.component'
+import { appRoutes } from './app.routes';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageNotFoundComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+    ),
     BrowserModule,
     FormsModule,
     HttpModule
